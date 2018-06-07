e8ebf3e51b3f45d1b646212ebedfee0f616f2040
Unknown <lorenzo.stacchio.dev@gmail.com>
2018-06-07 13:21:47 +0200

    Screen settings

--- app.py
+++ app.py
@@ -6,26 +6,51 @@ from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.uix.screenmanager  import ScreenManager,Screen
+from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.properties import StringProperty,ObjectProperty
+# Screen
 
+PATH_OPENFISCA = ""
 
-class HomeScreen(PageLayout):
+class InitScreen(Screen):
 
-    def __init__(self, **kwargs):
-        # make sure we aren't overriding any important functionality
-        super(HomeScreen, self).__init__(**kwargs)
+    def __init__(self,**kwargs):
+        super(InitScreen, self).__init__(**kwargs)
+        Clock.schedule_once(self._finish_init)
+
+    def _finish_init(self, dt):
+        #Builder.load_file("screens\init_screen_folder\init_screen_body.kv")
         self.ids.home_file_chooser.path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 
-    # to access elements in .kv self.ids.idrobatua
     def selected_file(self,*args): #in args ci sta il filepath di openfisca scelto
-        try: print args[1][0]
+        try:
+            PATH_OPENFISCA = args[1][0]
+            print "Inside try",PATH_OPENFISCA
+            self.manager.get_screen('home').ids.btn_boh.text = PATH_OPENFISCA
+            if self.manager.current == 'init':
+                 self.manager.current = 'home'
         except: pass
 
-class openfisca_managing_tool(App):
-    def build(self): #metodo che ci sta sempre nell'app
-        self.load_kv('screens\home_screen.kv')
-        return HomeScreen()
 
+class HomeScreen(Screen):
+    def __init__(self,**kwargs):
+        super(HomeScreen, self).__init__(**kwargs)
+        #print self.ids.btn_boh.text
+        #print self.manager
+        
+
+
+class MyScreenManager(ScreenManager):
+    pass
+
+# App
+class openfisca_managing_tool(App):
+    def build(self):
+        Builder.load_file('app.kv')
+        return MyScreenManager()
+
+# main
 if __name__ == '__main__':
     openfisca_managing_tool().run()
