832481405749525e3e94ba4531494fa5d541cc23
Unknown <lorenzo.stacchio.dev@gmail.com>
2018-06-19 16:42:48 +0200

    Improved simulation class

--- script/Simulation/Situation_for_simulation.py
+++ script/Simulation/Situation_for_simulation.py
@@ -105,99 +105,166 @@ class Entity():
 
 class Situation(): # defined for one entity
 
-    def __init__(self, name_of_situation = None , entity_choose = None, choosen_input_variables = None, choosen_output_variables = None, period = None):
+    def __init__(self, name_of_situation = "situazione_prova"):
         self.name_of_situation = name_of_situation
-        self.period = period
-        self.entity_choose = entity_choose
-        self.choosen_input_variables= choosen_input_variables
-        self.choosen_output_variables = choosen_output_variables
+        self.choosen_input_variables = None
+        self.choosen_output_variables = None
+        self.period = None
+
 
     def __repr__(self):
         return "\nSituation name: " + str(self.name_of_situation) + "\nSituation period: " + str(self.period) + "\nNumber of input variables: " + str(len(self.choosen_input_variables)) + "\nNumber of output variables: " + str(len(self.choosen_output_variables))
 
     def set_entity_choose(self, entity_choose):
-        for element in entity_choose: # we want a list
-            if not(element is Persona) and not(element is Famiglia):
+        if isinstance(entity_choose, Entity):
+            if not(entity_choose.entity is Persona) and not(entity_choose.entity is Famiglia):
                 raise ValueError("One of the inserted entity doesn't exist")
-        self.entity_choose = entity_choose # if we are here all it's ok
+            else:
+                self.entity_choose = entity_choose
+        else:
+            raise TypeError("The object used is not an entity")
 
-    # getters and setters
-    def set_period(self,period):
-        self.period = period
+    def set_period(self,year = None, month = None, day = None):
+        if year and not month and not day:
+            if not (re.match(r'.*([1-3][0-9]{3})', year) and len(year) == 4):
+                raise ValueError("No valid date selected")
+            else:
+                self.period = year
+        elif year and month and not day:
+            if not (re.match(r'.*([1-3][0-9]{3})', year) and len(year) == 4) or not(re.match(r'.*([1-12])', month) and len(month) == 2):
+                raise ValueError("No valid date selected")
+            else:
+                self.period = year + "-" + month
+        else:
+            raise ValueError("No valid date selected")
+        print self.period
 
     def get_period(self):
         return self.period
 
-    # TODO: controlla se le variabili messe in input esistono
-    def set_choosen_input_variables(self,choosen_input_variables):
-        if self.choosen_input_variables is None:
-            self.choosen_input_variables = {}
-        self.choosen_input_variables = choosen_input_variables
 
     def get_choosen_input_variables(self):
         return self.choosen_input_variables
 
-    def add_variable_to_choosen_input_variables(self,choosen_input_variable,value):
+
+    def add_variable_to_choosen_input_variables(self, choosen_input_variable, value):
         if self.choosen_input_variables is None:
             self.choosen_input_variables = {}
-        self.choosen_input_variables[choosen_input_variable] = value
+        variable_match = False
+        for entity, associated_variables in self.entity_choose.get_associated_variables().iteritems():
+            for variable in associated_variables:
+                #print "Variable exist", variable.name
+                #print "Variable choose", choosen_input_variable
+                if str(variable.name) == choosen_input_variable:
+                    self.choosen_input_variables[choosen_input_variable] = value
+                    variable_match = True
+                    break
+            if variable_match:
+                break
+        if not variable_match:
+            raise TypeError("The variable you choose, is not defined for the entity or doesn't exist")
 
-    def set_choosen_output_variables(self,choosen_output_variables):
-        if self.choosen_output_variables is None:
-            self.choosen_output_variables = []
-        self.choosen_output_variables = choosen_output_variables
 
     def get_choosen_output_variables(self):
         return self.choosen_output_variables
 
-    def add_variable_to_choosen_output_variables(self,choosen_output_variable):
+
+    def add_variable_to_choosen_output_variables(self, choosen_output_variable):
         if self.choosen_output_variables is None:
             self.choosen_output_variables = []
-        self.choosen_output_variables.append(choosen_output_variable)
+        variable_match = False
+        for entity, associated_variables in self.entity_choose.get_associated_variables().iteritems():
+            for variable in associated_variables:
+                #print "Variable exist", variable.name
+                #print "Variable choose", choosen_input_variable
+                if str(variable.name) == choosen_output_variable:
+                    self.choosen_output_variables.append(choosen_output_variable)
+                    variable_match = True
+                    break
+            if variable_match:
+                break
+        if not variable_match:
+            raise TypeError("The variable you choose, is not defined for the entity or doesn't exist")
 
 
 class Simulation_generator(): #defined for Italy
 
-    def __init__(self, situations = None, results = None, period = datetime.datetime.now().year):
-        self.situations = situations #take n situations
-        self.period = period
-        self.results = results
+
+    def __init__(self):
+        self.situations = None #take n situations
+        self.period = datetime.datetime.now().year
+        self.results = None
 
 
-    def init_profile(self, scenario, entity_situation):
+    def init_profile(self, scenario, situation_period, entity_situation):
+        print situation_period
+        print entity_situation
         #print "Situation: ", entity_situation
         scenario.init_single_entity(
-            period = self.period,
+            period = situation_period,
             parent1 = entity_situation
         )
         return scenario
 
-    def add_to_result(self,result):
-        if self.results is None:
-            self.results = []
-        self.results.append(result)
+    def set_period(self,year = None, month = None, day = None):
+        if year and not month and not day:
+            if not (re.match(r'.*([1-3][0-9]{3})', year) and len(year) == 4):
+                raise ValueError("No valid date selected")
+            else:
+                self.period = year
+        elif year and month and not day:
+            if not (re.match(r'.*([1-3][0-9]{3})', year) and len(year) == 4) or not(re.match(r'.*([1-12])', month) and len(month) == 2):
+                raise ValueError("No valid date selected")
+            else:
+                self.period = year + "-" + month
+        else:
+            raise ValueError("No valid date selected")
+        print self.period
+
+
+    def add_to_result(self, situation, name_of_variable_calculated, result):
+        if self.results is None: # initialize if it is empty
+            self.results = {}
+        if not situation in self.results:
+            self.results[situation] = {} #initialize inner dict
+        self.results[situation][name_of_variable_calculated] = result
+
 
     def get_results(self):
         return self.results
 
+
+    def add_situation_to_simulator(self, situation): # you can add situations with the same content
+        if self.situations is None:
+            self.situations = []
+        if isinstance(situation, Situation):
+            self.situations.append(situation)
+        else:
+            raise TypeError("The situation selected is not a valid situation")
+
+
     def generate_simulation(self):
         # import dinamically
         tax_benefit_system = italy_taxbenefitsystem.ItalyTaxBenefitSystem()
-        for situation in self.situations:
-            # RICORDA CHE DEVI FARE PRATICAMETE UNA SIMULAZIONE PER OGNI PERSONA, NEL SENSO CHE INIZIALIZZI TRE VOLTE LO SCENARIO E POI RUNNI per il problema dello scenario
-            scenario = tax_benefit_system.new_scenario()
-            scenario = self.init_profile(scenario = scenario, entity_situation = situation.get_choosen_input_variables())
-            simulation = scenario.new_simulation() # nuova simulazione per lo scenario normale
-            for element in situation.get_choosen_output_variables():
-                self.add_to_result(simulation.calculate(element,self.period))
+        if not (self.situations == []) and not (self.situations is None):
+            for situation in self.situations:
+                # RICORDA CHE DEVI FARE PRATICAMETE UNA SIMULAZIONE PER OGNI PERSONA, NEL SENSO CHE INIZIALIZZI TRE VOLTE LO SCENARIO E POI RUNNI per il problema dello scenario
+                scenario = tax_benefit_system.new_scenario()
+                scenario = self.init_profile(scenario = scenario, situation_period = situation.get_period(), entity_situation = situation.get_choosen_input_variables())
+                simulation = scenario.new_simulation() # nuova simulazione per lo scenario normale
+                for element in situation.get_choosen_output_variables():
+                    self.add_to_result(situation = situation.name_of_situation, name_of_variable_calculated = element , result = simulation.calculate(element,self.period))
+        else:
+            raise ValueError("To trigger a simulation, at least a situation it's needed")
+
 # main
 path = 'C:\\Users\\Stach\\Desktop\\openfisca-italy\\openfisca_italy'
-situation_IRPEF_1 = Situation(name_of_situation = "IRPEF_2017", period = '2017')
-person = Entity(entity = Persona) # LOAD PERSONA
-person.generate_associated_variable_filter(year = '2017' , month ='01' )
-#person.generate_associated_variable_filter(year = '2017')
-print person.get_associated_variables()
-#print "*************************PERSON***********************************"
+person = Entity(entity = Persona)
+person.generate_associated_variable_filter(year = '2017')
+
+situation_IRPEF_1 = Situation(name_of_situation = "IRPEF1_2017")
+situation_IRPEF_1.set_entity_choose(person)
+situation_IRPEF_1.set_period(year = '2017')
 # input variables
 situation_IRPEF_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'RN1_reddito_complessivo', value = 25000)
 situation_IRPEF_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'RN2_deduzione_abitazione_principale', value = 2000)
@@ -208,27 +275,45 @@ situation_IRPEF_1.add_variable_to_choosen_input_variables(choosen_input_variable
 situation_IRPEF_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'RN4_reddito_imponibile')
 situation_IRPEF_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'RN5_irpef_lorda')
 situation_IRPEF_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'RN26_irpef_netta')
-
 print "IRPEF",  situation_IRPEF_1
-print id(situation_IRPEF_1)
-situation_IMU_1 = Situation(name_of_situation = "IMU_2017", period = '2017-01')
-situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'valore_immobile_non_rivalutato', value = 10000)
-situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'is_immobile_prima_casa', value = False)
-situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'immobile_categoria_catastale', value = 'A5')
-situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'percentuale_possesso', value = 100)
-situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'mesi_di_possesso', value = 12)
-situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'aliquota_imu', value = 7.6)
-situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'is_interesse_storico_artistico', value = True)
-# outpu
-situation_IMU_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'imposta_imu')
-situation_IMU_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'detrazioni_imu')
-situation_IMU_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'importo_imu')
-print "IMU",  situation_IMU_1
-print id(situation_IMU_1)
 
-simulation_IRPEF = Simulation_generator(situations = [situation_IRPEF_1], period='2017')
+
+situation_IRPEF_2 = Situation(name_of_situation = "IRPEF2_2017")
+situation_IRPEF_2.set_entity_choose(person)
+situation_IRPEF_2.set_period(year = '2017')
+# input variables
+situation_IRPEF_2.add_variable_to_choosen_input_variables(choosen_input_variable = 'RN1_reddito_complessivo', value = 15000)
+situation_IRPEF_2.add_variable_to_choosen_input_variables(choosen_input_variable = 'RN2_deduzione_abitazione_principale', value = 1000)
+situation_IRPEF_2.add_variable_to_choosen_input_variables(choosen_input_variable = 'RN3_oneri_deducibili_totali', value = 1000)
+situation_IRPEF_2.add_variable_to_choosen_input_variables(choosen_input_variable = 'RN22_totale_detrazioni_imposta', value = 500)
+situation_IRPEF_2.add_variable_to_choosen_input_variables(choosen_input_variable = 'RN25_totale_altre_detrazioni_crediti_imposta', value = 400)
+# output
+situation_IRPEF_2.add_variable_to_choosen_output_variables(choosen_output_variable = 'RN4_reddito_imponibile')
+situation_IRPEF_2.add_variable_to_choosen_output_variables(choosen_output_variable = 'RN5_irpef_lorda')
+situation_IRPEF_2.add_variable_to_choosen_output_variables(choosen_output_variable = 'RN26_irpef_netta')
+print "IRPEF",  situation_IRPEF_2
+#print id(situation_IRPEF_1)
+#situation_IMU_1 = Situation(name_of_situation = "IMU_2017")
+#situation_IMU_1.set_period(year = '2017' , month ='01')
+#situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'valore_immobile_non_rivalutato', value = 10000)
+#situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'is_immobile_prima_casa', value = False)
+#situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'immobile_categoria_catastale', value = 'A5')
+#situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'percentuale_possesso', value = 100)
+#situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'mesi_di_possesso', value = 12)
+#situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'aliquota_imu', value = 7.6)
+#situation_IMU_1.add_variable_to_choosen_input_variables(choosen_input_variable = 'is_interesse_storico_artistico', value = True)
+# outpu
+#situation_IMU_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'imposta_imu')
+#situation_IMU_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'detrazioni_imu')
+#situation_IMU_1.add_variable_to_choosen_output_variables(choosen_output_variable = 'importo_imu')
+#print "IMU",  situation_IMU_1
+#print id(situation_IMU_1)
+simulation_IRPEF = Simulation_generator()
+simulation_IRPEF.set_period('2017')
+simulation_IRPEF.add_situation_to_simulator(situation_IRPEF_1)
+simulation_IRPEF.add_situation_to_simulator(situation_IRPEF_2)
 simulation_IRPEF.generate_simulation()
 print simulation_IRPEF.get_results()
-simulation_IMU = Simulation_generator(situations = [situation_IMU_1], period='2017-01')
-simulation_IMU.generate_simulation()
-simulation_IMU.get_results()
+#simulation_IMU = Simulation_generator(situations = [situation_IMU_1], period='2017-01')
+#simulation_IMU.generate_simulation()
+#simulation_IMU.get_results()
