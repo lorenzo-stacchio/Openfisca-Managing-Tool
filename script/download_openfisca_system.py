fdf43033e3776865170860cc550a13368922e248
Unknown <lorenzo.stacchio.dev@gmail.com>
2018-06-21 20:49:23 +0200

    Fix pathname bug

--- script/download_openfisca_system.py
+++ script/download_openfisca_system.py
@@ -7,21 +7,24 @@ from subprocess import check_output
 # TODO: message if directory exist yet
 def download_and_install(path_to_save, project_name, github_link):
     current_path = os.getcwd()
+    full_path = str(path_to_save + "\\"+ project_name)
     try:
-        if os.path.exists(path_to_save + "\\"+ project_name):
-            os.remove((path_to_save + "\\"+ project_name))
-        os.chdir(path_to_save)
+        if os.path.exists(full_path):
+            os.remove((full_path))
+        os.chdir(str(path_to_save))
         check_output("git clone " + github_link, shell=True).decode()
 
         check_output("python -m pip install --upgrade pip", shell=True).decode()
 
-        os.chdir(path_to_save + "\\"+ project_name)
+        os.chdir(full_path)
 
         if project_name == 'openfisca-italy':
             check_output("git checkout Initizialize_open-fisca-italy", shell=True).decode()
-        check_output("pip install --editable " + path_to_save + "\\"+ project_name, shell=True).decode()
+        full_path = "\""+full_path+"\""
+        check_output("pip install --editable " + full_path, shell=True).decode()
         os.chdir(current_path)
         return True
-    except Exception:
+    except Exception as e:
+        print e
         os.chdir(current_path)
         return False
