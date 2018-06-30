9b641faa3a92570043e490445b1d5694cd7936b3
Unknown <corrado.petrelli@gmail.com>
2018-06-30 11:35:47 +0200

    Summary of entity inserted and check if i insert all values

--- folder_screen_widgets/personalized_widget.py
+++ folder_screen_widgets/personalized_widget.py
@@ -10,6 +10,11 @@ from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
 from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.uix.modalview import ModalView
+from kivy.uix.scrollview import ScrollView
+from kivy.metrics import dp
+from kivy.graphics import Rectangle, Color
 
 
 class LineOfChooser(BoxLayout):
@@ -72,3 +77,134 @@ class ConfirmPopup(GridLayout):
 
     def on_answer(self, *args):
         pass
+
+
+class Pop(ModalView):
+    list=[]
+    index = 0
+    def __init__(self, title, list=[""], callback=None,
+                 width=None, height=None, **kwargs):
+        super(Pop, self).__init__(**kwargs)
+
+        self.list = list
+        self.callback = callback
+        self.auto_dismiss = False
+        self.size_hint = (None, None)
+        self.preferred_width = width
+        self.preferred_height = height
+
+        if self.preferred_width:
+            self.width = self.preferred_width
+        elif Window.width > 500:  # Big screen?
+            self.width = 0.7 * Window.width
+        else:
+            self.width = Window.width - 2
+
+        self.playout = BoxLayout(orientation='vertical',
+                                 padding=["2dp", "5dp",
+                                          "2dp", "5dp"],
+                                 spacing="5dp")
+
+        self.title = Label(size_hint_y=None,
+                           text_size=(self.width - dp(20), None),
+                           text=title,
+                           halign='left',
+                           font_size="16sp",
+                           markup=True)
+
+        self.separator = BoxLayout(size_hint_y=None, height="1dp")
+
+        self.pscroll = ScrollView(do_scroll_x=False)
+
+        self.content = Label(size_hint_y=None,
+                             text=self.list[0],
+                             halign='justify',
+                             font_size="16sp",
+                             markup=True,
+                             text_size=(self.width- dp(20), None))
+
+        self.pbutton = Button(text='Close',
+                              size_hint_y=None, height="18dp")
+        self.pbutton.bind(on_release=self.close)
+
+        self.buttonforward = Button(text='<',
+                              size_hint_y=None, height="18dp")
+
+        self.buttonforward.bind(on_release=self.minus_one_index)
+        self.buttonbackward = Button(text='>',
+                              size_hint_y=None, height="18dp")
+        self.buttonbackward.bind(on_release=self.plus_one_index)
+
+        self.boxlayout_button = BoxLayout(orientation="horizontal")
+        self.add_widget(self.playout)
+        self.playout.add_widget(self.title)
+        self.playout.add_widget(self.separator)
+        self.playout.add_widget(self.pscroll)
+        self.pscroll.add_widget(self.content)
+        self.boxlayout_button.add_widget(self.buttonforward)
+        self.boxlayout_button.add_widget(self.buttonbackward)
+        self.boxlayout_button.add_widget(self.pbutton)
+        self.playout.add_widget(self.boxlayout_button)
+
+        self.title.bind(texture_size=self.update_height)
+        self.content.bind(texture_size=self.update_height)
+
+        with self.separator.canvas.before:
+            Color(1, 1, 1, 1)
+            self.rect = Rectangle(pos=self.separator.pos,
+                                  size=self.separator.size)
+
+        self.separator.bind(pos=self.update_sep,
+                            size=self.update_sep)
+
+        Window.bind(size=self.update_width)
+
+    def update_width(self, *args):
+        # hack to resize dark background on window resize
+        self.center = Window.center
+        self._window = None
+        self._window = Window
+
+        if self.preferred_width:
+            self.width = self.preferred_width
+        elif Window.width > 500:  # Big screen?
+            self.width = 0.7 * Window.width
+        else:
+            self.width = Window.width - 2
+
+        self.title.text_size = (self.width - dp(20), None)
+        self.content.text_size = (self.width - dp(20), None)
+
+    def update_height(self, *args):
+        self.title.height = self.title.texture_size[1]
+        self.content.height = self.content.texture_size[1]
+        temp = self.title.height + self.content.height + dp(56)
+        if self.preferred_height:
+            self.height = self.preferred_height
+        elif temp > Window.height - dp(40):
+            self.height = Window.height - dp(40)
+        else:
+            self.height = temp
+        self.center = Window.center
+
+    def update_sep(self, *args):
+        self.rect.pos = self.separator.pos
+        self.rect.size = self.separator.size
+
+    def minus_one_index(self, instance):
+        if(self.index != 0):
+            self.index-=1
+        else:
+            self.index = len(self.list)-1
+        self.content.text = self.list[self.index]
+    def plus_one_index(self, instance):
+        if(len(self.list)-1 != self.index):
+            self.index += 1
+        else:
+            self.index = 0
+        self.content.text = self.list[self.index]
+    def close(self, instance):
+        self.dismiss(force=True)
+        if self.callback:
+            self.callback()
+
